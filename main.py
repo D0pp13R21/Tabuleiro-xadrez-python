20bd756fea68fabc598b0212e816e2d7e5a8f2b5
D0pp13R21 <marinho.dev98@gmail.com>
2023-01-16 22:35:25 -0300

    Programa finalizado

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
 
-tabuleiro = np.title([1,0], [8,4])
+tabuleiro = np.tile([1,0], [8,4])
 
 for i in range(tabuleiro.shape[0]):
     tabuleiro[i] = np.roll(tabuleiro[i], i % 2)
 
-mapa_de_cores = ListedColormap(['#779556', '#ebecd0'])
+mapa_de_cores = ListedColormap(['#000000', '#ffffff'])
 plt.matshow(tabuleiro, cmap=mapa_de_cores)
 plt.xticks([])
 plt.yticks([])
